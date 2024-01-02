1b06d41d0b3610543cad00d20e7f4b29cae3f417
AlejandroLuisHC <alejandrolhc@gmail.com>
2024-01-02 18:05:21 +0100

    Add Vite dependencies and update Spotify API authentication

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,17 +1,21 @@
-import SpotifyWebApi from 'spotify-web-api-node';
+import SpotifyWebApi from "spotify-web-api-node";
 
 export const spotifyApi = new SpotifyWebApi();
 
-const accessToken = await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: "grant_type=client_credentials&client_id=52f7fd0717ba4545bd1491fe26f0fc80&client_secret=db0d5f2131864ccaa34ce73f26ce64b8",
-})
-    .then(res => res.json())
-    .then(res => res.access_token);
+const getAccessToken = async () => {
+    const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: "grant_type=client_credentials&client_id=52f7fd0717ba4545bd1491fe26f0fc80&client_secret=db0d5f2131864ccaa34ce73f26ce64b8",
+    });
 
-spotifyApi.setAccessToken(accessToken);
+    const data = await response.json();
+    return data.access_token;
+};
 
-
+(async () => {
+    const accessToken = await getAccessToken();
+    spotifyApi.setAccessToken(accessToken);
+})();
